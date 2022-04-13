0f7b24ba44aae5eeea588bbdf93820155acecd2d
Wooyoung Kim <wooyoungkim24@gmail.com>
2022-04-13 13:53:38 -0400

    lunch wed

--- backend/routes/api/servers.js
+++ backend/routes/api/servers.js
@@ -7,26 +7,26 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 const { Op } = require('sequelize');
-const {Member, Admin, Moderator, Server, TextChannel, VoiceChannel} = require('../../db/models')
+const { Member, Admin, Moderator, Server, TextChannel, VoiceChannel } = require('../../db/models')
 
 //Servers
-router.get("/all/:id", asyncHandler(async(req,res) =>{
+router.get("/all/:id", asyncHandler(async (req, res) => {
     const userId = req.params.id;
 
     const members = await Member.findAll({
-        where:{
+        where: {
             userId
         },
         include: Server
     })
     const moderators = await Moderator.findAll({
-        where:{
+        where: {
             userId
         },
         include: Server
     })
     const admins = await Admin.findAll({
-        where:{
+        where: {
             userId
         },
         include: Server
@@ -39,31 +39,69 @@ router.get("/all/:id", asyncHandler(async(req,res) =>{
 }))
 
 //Create new server and adds admin
-router.post("/", asyncHandler(async(req,res) =>{
-    const {userId} = req.body
+router.post("/", asyncHandler(async (req, res) => {
+    const { userId } = req.body
     const newServer = await Server.create(req.body)
     const newServerId = newServer.id
     const newAdmin = await Admin.create({
         userId,
         serverId: newServerId
     })
-    return res.json({newServer,newAdmin})
+    return res.json({ newServer, newAdmin })
 }))
 
+router.put("/", asyncHandler(async (req, res) => {
+    const { id } = req.body;
+    const updateServer = await Server.findByPk(id)
+    const updatedServer = await updateServer.update(req.body)
+    return res.json(updatedServer)
+}))
+
+router.delete("/", asyncHandler(async (req, res) => {
+    const { id, userId } = req.body
+    const deletedServer = await Server.findByPk(id)
+    await Server.destroy({
+        where: {
+            id: id
+        }
+    })
+    await Admin.destroy({
+        where: {
+            [Op.and]: [
+                { serverId: id },
+                { userId: userId }
+            ]
+        }
+    })
+    return res.json(deletedServer)
+}))
+
+
+
 //New Member
-router.post("/new/member", asyncHandler(async(req,res) =>{
+router.post("/new/member", asyncHandler(async (req, res) => {
     const newMember = await Member.create(req.body)
     return res.json(newMember)
 }))
+router.delete("/delete/member", asyncHandler(async (req, res) => {
+    const { id } = req.body
+    const deletedMember = await Member.findByPk(id)
+    await Member.destroy({
+        where: {
+            id: id
+        }
+    })
+    return res.json(deletedMember)
+}))
 
-router.post("/demote/member", asyncHandler(async(req,res) =>{
+router.post("/demote/member", asyncHandler(async (req, res) => {
 
-    const {userId, serverId} = req.body
+    const { userId, serverId } = req.body
     await Moderator.destroy({
-        where:{
-            [Op.and]:[
-                {userId:userId},
-                {serverId:serverId}
+        where: {
+            [Op.and]: [
+                { userId: userId },
+                { serverId: serverId }
             ]
         }
     })
@@ -72,13 +110,13 @@ router.post("/demote/member", asyncHandler(async(req,res) =>{
 }))
 
 
-router.post("/new/moderator", asyncHandler(async(req,res) =>{
-    const {userId, serverId} = req.body;
+router.post("/new/moderator", asyncHandler(async (req, res) => {
+    const { userId, serverId } = req.body;
     await Member.destroy({
-        where:{
-            [Op.and]:[
-                {userId:userId},
-                {serverId:serverId}
+        where: {
+            [Op.and]: [
+                { userId: userId },
+                { serverId: serverId }
             ]
         }
     })
@@ -86,45 +124,81 @@ router.post("/new/moderator", asyncHandler(async(req,res) =>{
     return res.json(newModerator)
 }))
 
+router.post("/delete/moderator", asyncHandler(async(req,res)=>{
+    const {id} = req.body
+    const oldModerator = await Moderator.findByPk(id)
+    await Moderator.destroy({
+        where:{
+            id:id
+        }
+    })
+    return res.json(oldModerator)
+}))
+
 
 //Text Channels
-router.get("/all/text/:id", asyncHandler(async(req,res) =>{
+router.get("/all/text/:id", asyncHandler(async (req, res) => {
     const serverId = req.params.id
     const textChannels = await TextChannel.findAll({
-        where:{
+        where: {
             serverId
         }
     })
     return res.json(textChannels)
 }))
-router.post("/text", asyncHandler(async(req,res)=>{
+router.post("/text", asyncHandler(async (req, res) => {
     const newTextChannel = await TextChannel.create(req.body)
     return res.json(newTextChannel)
 }))
-router.put("/text", asyncHandler(async(req,res)=>{
-    const {id} = req.body
+router.put("/text", asyncHandler(async (req, res) => {
+    const { id } = req.body
     const updateText = await TextChannel.findByPk(id);
     const updatedText = await updateText.update(req.body)
     return res.json(updatedText)
 }))
+router.delete("/text", asyncHandler(async (req, res) => {
+    const { id } = req.body
+    let deletedChannel = await TextChannel.findByPk(id)
+    await TextChannel.destroy({
+        where: {
+            id: id
+        }
+    })
+    return res.json(deletedChannel)
+}))
 
 
 //Voice Channels
-router.get("/all/voice/:id", asyncHandler(async(req,res) =>{
+router.get("/all/voice/:id", asyncHandler(async (req, res) => {
     const serverId = req.params.id
     const voiceChannels = await VoiceChannel.findAll({
-        where:{
+        where: {
             serverId
         }
     })
     return res.json(voiceChannels)
 }))
-router.post("/voice", asyncHandler(async(req,res)=>{
+router.post("/voice", asyncHandler(async (req, res) => {
     const newVoiceChannel = await VoiceChannel.create(req.body)
     return res.json(newVoiceChannel)
 }))
 
-
+router.put("/voice", asyncHandler(async (req, res) => {
+    const { id } = req.body
+    const updateVoice = await VoiceChannel.findByPk(id);
+    const updatedVoice = await updateText.update(req.body)
+    return res.json(updatedVoice)
+}))
+router.delete("/voice", asyncHandler(async (req, res) => {
+    const { id } = req.body
+    let deletedChannel = await VoiceChannel.findByPk(id)
+    await VoiceChannel.destroy({
+        where: {
+            id: id
+        }
+    })
+    return res.json(deletedChannel)
+}))
 
 
 
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,10 +2,15 @@ const express = require('express')
 const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const { User, UserFriend } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { Op } = require('sequelize');
+
 const router = express.Router();
+
+
+
 const validateSignup = [
     check('email')
         .exists({ checkFalsy: true })
@@ -39,4 +44,97 @@ router.post(
         });
     }),
 );
+router.get("/friends/:id", asyncHandler(async (req, res) => {
+    const id = req.params.id
+    const friends = await UserFriend.findAll({
+        where: {
+            [Op.or]: [
+                { friend1: id },
+                { friend2: id }
+            ]
+        }
+    })
+    let friendObjs = []
+    friends.forEach(ele => {
+        if (ele.friend1 === id) {
+            friendObjs.push(ele)
+        } else {
+            let currObj = {}
+            currObj["friend1"] = ele.friend2
+            currObj["friend2"] = ele.friend1
+            friendObjs.push(currObj)
+        }
+    })
+    return res.json(friendObjs)
+}))
+
+
+router.post("/new/friend", asyncHandler(async (req, res) => {
+    const { userId, friendId } = req.body
+    const allFriends = await UserFriend.findAll();
+    if (userId === friendId) {
+        throw new Error("Cannot friend yourself")
+    }
+    allFriends.forEach(ele => {
+        if (ele.friend1 === userId) {
+            if (ele.friend2 === friendId) {
+                throw new Error("Friendship already exists")
+            }
+        }
+        if (ele.friend1 === friendId) {
+            if (ele.friend2 === userId) {
+                throw new Error("Friendship already exists")
+            }
+        }
+    })
+    let newFriend = await UserFriend.create({
+        friend1: userId,
+        friend2: friendId
+    })
+    return res.json(newFriend)
+}))
+
+
+router.delete("/delete/friend", asyncHandler(async (req, res) => {
+    const { userId, friendId } = req.body
+    let destroyedFriend = await UserFriend.findOne({
+        where: {
+            [Op.or]: [
+                {
+                    [Op.and]: [
+                        { friend1: userId },
+                        { friend2: friendId }
+                    ]
+
+                },
+                {
+                    [Op.and]: [
+                        { friend1: friendId },
+                        { friend2: userId }
+                    ]
+                }
+            ]
+        }
+    })
+    await UserFriend.destroy({
+        where: {
+            [Op.or]: [
+                {
+                    [Op.and]: [
+                        { friend1: userId },
+                        { friend2: friendId }
+                    ]
+
+                },
+                {
+                    [Op.and]: [
+                        { friend1: friendId },
+                        { friend2: userId }
+                    ]
+                }
+            ]
+        }
+    })
+    return res.json(destroyedFriend)
+}))
 module.exports = router;
